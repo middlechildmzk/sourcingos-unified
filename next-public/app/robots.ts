96171439dc0036f5fa9833cd90c5adf9402970b0
middlechildmzk <realmiddlechildmzk@gmail.com>
2026-07-04 14:05:35 -0500

    V18 Phase 1-2: disallow jobs admin in robots

--- next-public/app/robots.ts
+++ next-public/app/robots.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from 'next'
 import { siteUrl } from '@/lib/site'
 
 export default function robots(): MetadataRoute.Robots {
-  const privateRoutes = ['/admin/', '/app/', '/login/']
+  const privateRoutes = ['/admin/', '/app/', '/login/', '/jobs/admin/']
   return {
     rules: [{ userAgent: '*', allow: '/', disallow: privateRoutes }],
     sitemap: `${siteUrl}/sitemap.xml`,
