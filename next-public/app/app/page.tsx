d453b86c6245ec25f1e1fdea93c0fea69401431f
middlechildmzk <realmiddlechildmzk@gmail.com>
2026-07-20 07:04:17 -0500

    Make Role Workspaces the private app home

--- next-public/app/app/page.tsx
+++ next-public/app/app/page.tsx
@@ -2,6 +2,6 @@ import { redirect } from 'next/navigation'
 
 export const metadata = { title: 'SourcingOS App', robots: { index: false, follow: false } }
 
-export default function Page(){
-  redirect('/app/candidate-search')
+export default function Page() {
+  redirect('/app/roles')
 }
