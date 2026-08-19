03061ec9eccb90e5b7b88185c29cc8e808a390ec
middlechildmzk <realmiddlechildmzk@gmail.com>
2026-08-19 10:17:00 -0500

    Preserve LinkedIn Recruiter SEO equity with redirects

--- next-public/next.config.mjs
+++ next-public/next.config.mjs
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true
+  trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/linkedin-recruiter-alternative',
+        destination: '/blog/linkedin-recruiter-alternatives/',
+        permanent: true,
+      },
+      {
+        source: '/linkedin-recruiter',
+        destination: '/blog/linkedin-recruiter-alternatives/',
+        permanent: true,
+      },
+    ]
+  },
 }
 export default nextConfig
