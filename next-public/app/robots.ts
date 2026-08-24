ecf5bf250aff47fef14655e8b6b7672b5e6b1e10
middlechildmzk <realmiddlechildmzk@gmail.com>
2026-08-24 10:59:10 -0500

    Keep partner redirect routes out of search

--- next-public/app/robots.ts
+++ next-public/app/robots.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from 'next'
 import { siteUrl } from '@/lib/site'
 
 export default function robots(): MetadataRoute.Robots {
-  const privateRoutes = ['/admin/', '/app/', '/login/', '/jobs/admin/']
+  const privateRoutes = ['/admin/', '/app/', '/login/', '/jobs/admin/', '/go/']
   return {
     rules: [{ userAgent: '*', allow: '/', disallow: privateRoutes }],
     sitemap: `${siteUrl}/sitemap.xml`,
